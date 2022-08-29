ee5ad9812e42fe535c006b51c99885a2c992f045
Aditya <aditya18.mail@gmail.com>
2022-08-29 19:21:27 +0530

    task5

--- Dom/dom.js
+++ Dom/dom.js
@@ -16,9 +16,19 @@ let add_item = document.getElementById('add');
 add_item.style.color='green';
 add_item.style.fontWeight='bold';
 let items = document.getElementsByClassName('list-group-item');
-items[1].textContent='hello 2';
+/*items[1].textContent='hello 2';
 items[2].textContent='hello 3';
 for(let i=0;i<items.length;i++){
     items[i].style.background='green';
     items[i].style.fontWeight='bold';
+}
+*/
+
+let tagname = document.getElementsByTagName('li');
+tagname[1].textContent='hello 2';
+tagname[2].textContent='hello 3';
+
+for(let i=0;i<items.length;i++){
+    tagname[i].style.background='green';
+    tagname[i].style.fontWeight='bold';
 }
