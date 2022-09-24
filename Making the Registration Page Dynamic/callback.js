0cb806a8b42cb4ac2fab4c1814c86b515dc67f61
Aditya <aditya18.mail@gmail.com>
2022-09-24 22:26:40 +0530

    callback

--- Making the Registration Page Dynamic/callback.js
+++ Making the Registration Page Dynamic/callback.js
@@ -1,52 +1,47 @@
-const posts=[
-    {title:'Post One',body:'this is post one',createAt:new Date().getTime()},
-    {title:'Post Two',body:'this is post two',createAt: new Date().getTime()}
+const posts =[
+    {title : 'Post One',body:'This is Post one',createdAt: new Date().getTime()},
+    {title:'Post Two',body:'This is post two',createdAt: new Date().getTime()}
 ];
-
-
-let intervalId = 0;
-
+let intervalId=0;
 function getPost(){
     clearInterval(intervalId);
-    intervalId=setInterval(()=>{
-        
-let output = '';
-for(let i=0;i<posts.length;i++){
-    output+=`<li>${posts[i].title} - last updated ${(new Date().getTime()-posts[i].createAt)/1000}</li>`
+    intervalId= setInterval(()=>{
+        let output='';
+        for(let i=0;i<posts.length;i++){
+            output += `<li>${posts[i].title} - last updated ${(new Date().getTime() - posts[i].createdAt) / 1000} Seconds ago</li>`
+        }
+        document.body.innerHTML =output;
 
-    } 
-    console.log('timer running id=',intervalId);  
-    document.body.innerHTML= output;
-    },1000);
+    },1000)
+    
 }
 
-
-
- function createPost(post,callback){
+function createPost(post,callback){
     setTimeout(()=>{
-        posts.push({ ...post, createdAt: new Date().getTime()});
-        callback();
-    },2000);
+        posts.push({...post, createdAt: new Date().getDate()});
+        callback()
+    },2000)
 }
 
-
-function createPost2(post,callback){
+function create4thPost(post,callback){
     setTimeout(()=>{
-posts.push({ ...post,createdAt: new Date().getTime()});
-callback();
+        posts.push({...post, createdAt: new Date().getDate()});
+        callback()
     },6000)
 }
-getPost();
-createPost({title:'Post Three',body:'This is Post'},getPost)
-createPost2({title:'Post Four',body:'this is Post'},getPost)
+
+getPost()
+createPost({title:'Post Three',body:'this is post three'},getPost);
+create4thPost({title:'Post Four',body:'this is post four'},getPost);
+
 
 var timer;
 var count=0;
-function lastEditedinSecondAgo(){
-    count=0;
+function lastEditedinSecondsAgo(){
+    count =0;
     clearInterval(timer)
-    timer = setInterval(()=>{
+    timer= setInterval(()=>{
         count++;
-        console.log(count);
-    },5000)
+        console.log(count)
+    },5000);
 }
