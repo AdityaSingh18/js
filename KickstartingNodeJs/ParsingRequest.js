54079fab45c748541907edb9d759e002bc7dc76b
Aditya <aditya18.mail@gmail.com>
2022-10-25 20:30:23 +0530

    ParsingRequest

--- KickstartingNodeJs/ParsingRequest.js
+++ KickstartingNodeJs/ParsingRequest.js
@@ -4,13 +4,21 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
+ if(url === "/"){
+    fs.readFile("message.txt", {encoding: "utf-8"},(err,data)=>{
+        if(err){
+            console.log(err)
+        }
+
+        console.log(`data from file`+data);
+        res.write("<html>");
+        res.write("<head>title>Enter Message</title><head>");
+        res.write(`<body>${data}<>`)
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write("</html>")
+        return res.end();
+    })
+ }
   if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', (chunk) => {
