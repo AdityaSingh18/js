a380b4d4bc71466f8fda41d8ee087239cb4ca33b
Aditya <aditya18.mail@gmail.com>
2022-11-03 21:31:05 +0530

    Mysql

--- StartingWithExpress/Ecommerce/app.js
+++ StartingWithExpress/Ecommerce/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
+const db = require('./util/database')
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,6 +17,8 @@ app.set('views',viewspath)
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
