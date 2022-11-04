9692e6a749ec88d8818e48d17f8ab60ae7581448
Aditya <aditya18.mail@gmail.com>
2022-11-04 18:29:28 +0530

    Insert , update and Get all the products

--- StartingWithExpress/Ecommerce/util/database.js
+++ StartingWithExpress/Ecommerce/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete',
-    password: 1808
-})
+    password: '1808'
+});
 
 module.exports = pool.promise();
